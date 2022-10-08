97a9fda9817db75a2015fe94898ae78901558c91
Redwane Seghrouchni <redone.segh@gmail.com>
2022-10-08 21:17:13 +0200

    query video working

---
+++ main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+# Create a playlist in the user account with all the highlights of the games of the Week End
+
+import argparse
+import os
+import datetime
+
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+from sourceCode import *
+    
+CLIENT_SECRETS_FILE = 'code_secret_client_836432930376-9qe7hdo8pnsdbl92gt13n2gcq0fltkfg.apps.googleusercontent.com.json'
+
+SCOPES = ['https://www.googleapis.com/auth/youtube']
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+    
+# Authorize the request and store authorization credentials.
+def get_authenticated_service():
+  flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+  credentials = flow.run_console()
+  return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
+    
+
+
+if __name__ == '__main__':
+  youtube = get_authenticated_service()
+  while(1):
+  
+    playlist = create_Playlist(youtube)
+    video = search_highlights(youtube, teams)
+    fill_playlist(youtube, playlist, video)
+    
+    input()
+
---
+++ sourceCode.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+
+# Create a playlist in the user account with all the highlights of the games of the Week End
+
+import argparse
+import os
+import datetime
+
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+    
+CLIENT_SECRETS_FILE = 'code_secret_client_836432930376-9qe7hdo8pnsdbl92gt13n2gcq0fltkfg.apps.googleusercontent.com.json'
+
+SCOPES = ['https://www.googleapis.com/auth/youtube']
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+    
+# Authorize the request and store authorization credentials.
+def get_authenticated_service():
+  flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+  credentials = flow.run_console()
+  return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
+    
+def create_Playlist(youtube):
+  date = datetime.datetime.now(datetime.timezone.utc)
+  body = dict(
+    snippet=dict(
+      title='resume du week end' + date.isoformat(),
+      description='tout le meilleur contenu du week end'
+    ),
+    status=dict(
+      privacyStatus='private'
+    ) 
+  ) 
+
+  playlist = youtube.playlists().insert(
+    part='snippet,id,status',
+    body=body
+  ).execute()
+
+  return(playlist)
+
+teams = ['real madrid', 'barca']
+
+def search_highlights(youtube, teams):
+  videos = []
+  date = datetime.datetime.now(datetime.timezone.utc)
+  date = date + datetime.timedelta(days=-7)
+  default=date.isoformat()
+  for team in teams :
+    search_response = youtube.search().list(
+      q=team,
+      part='id,snippet',
+      maxResults=1,
+      publishedAfter=date.isoformat()
+    ).execute()
+    videos.append(search_response['items'][0]['id'])
+
+  print(videos)
+  return(videos)
+
+  
+
+
+def fill_playlist(youtube, playlist, videos):
+
+  for video in videos:
+    body = dict(
+      snippet=dict(
+        playlistId=playlist['id'],
+        resourceId=video
+      )
+    )
+    playlistItems_insert_response = youtube.playlistItems().insert(
+      part='id,snippet',
+      body=body
+        ).execute()
+
+
+  '''
+  
+    body=dict(
+      snippet=dict(
+        playlistId='PLU0gTL-vIMec56150WMYRqqbMdVNXbYtI',
+        resourceId=dict(
+          kind='youtube#video',
+          videoId='C_bihBdAmXk'
+        )
+      )
+    )'''
